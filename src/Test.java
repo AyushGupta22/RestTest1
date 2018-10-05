8f4c59ef55893a47a96f3e4c3287922dc3246b9e
AyushGupta22 <ayushgupta530@gmail.com>
2018-10-05 16:56:36 +0530

    First Commit

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/helloworld")
+public class Test {
+    @GET
+    @Produces("text/plain")
+    public String getClichedMessage() {
+        return "Hello World again";
+    }
+}
